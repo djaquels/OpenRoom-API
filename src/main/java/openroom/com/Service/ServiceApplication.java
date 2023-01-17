e94a3d32650e3351dbccf45dc498c8800e7e1828
Daniel Jacales <hjacales@gmail.com>
2023-01-17 16:57:10 -0600

    new files updated

--- src/main/java/openroom/com/Service/ServiceApplication.java
+++ src/main/java/openroom/com/Service/ServiceApplication.java
@@ -6,11 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "OpenRoom API", version = "2.0", description = "API Information"))
 @Configuration
 @EnableTransactionManagement(proxyTargetClass = true)
-@EnableJpaRepositories("openroom.com.Service.repositories")
+@EnableJpaRepositories("openroom.com.Service")
 public class ServiceApplication {
 
 	public static void main(String[] args) {
